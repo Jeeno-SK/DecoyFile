8315b8bb4f8fe6ba1b2abce84fffc2287ee0c2c7
Jeeno SK <jeenosk04@gmail.com>
2026-04-01 13:57:23 +0530

    Remove comments from main function

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "logic.h"
 
-// Declare monitor function
+
 void start_monitor();
 
 int main() {
     printf("Starting Decoy File Monitoring System...\n");
 
-    // Step 1: Load config
+
     load_config();
 
-    // Step 2: Start monitoring
+
     start_monitor();
 
     return 0;
